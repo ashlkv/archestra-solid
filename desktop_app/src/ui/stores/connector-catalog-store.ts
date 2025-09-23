a89a2186b7f6a63addae98696cc8240bf6251890
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2025-09-23 18:51:28 +0200

    feat: Whatsapp MCP, remove mock

--- desktop_app/src/ui/stores/connector-catalog-store.ts
+++ desktop_app/src/ui/stores/connector-catalog-store.ts
@@ -8,7 +8,6 @@ import {
   getMcpServerCategories,
   searchMcpServerCatalog,
 } from '@ui/lib/clients/archestra/catalog/gen';
-import mockWhatsapp from '@ui/lib/clients/archestra/catalog/gen/whatsapp-mock.json';
 
 /**
  * NOTE: ideally should be divisible by 3 to make it look nice in the UI (as we tend to have 3 "columns" of servers)
@@ -84,8 +83,6 @@ export const useConnectorCatalogStore = create<ConnectorCatalogStore>((set, get)
       }
 
       const { data } = await searchMcpServerCatalog({ query: params });
-      // FIXME Remove when whatsapp mcp catalog entry is updated
-      data.servers.unshift(mockWhatsapp);
 
       if (data) {
         let filteredLocalServers: ArchestraMcpServerManifest[] = [];
